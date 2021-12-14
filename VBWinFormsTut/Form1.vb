8d8d10bd0a84ac750bcec6952f0bdcba12045107
Marco Preciado <mac46005@gmail.com>
2021-12-14 10:05:40 -0600

    Using variables in vb

--- VBWinFormsTut/Form1.vb
+++ VBWinFormsTut/Form1.vb
@@ -1,5 +1,10 @@
 ﻿Public Class Form1
+
+
+    Dim Message As String = "This is a message string."
+    Dim integerValue As Integer = Message.Length
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("Hello World!")
+        MessageBox.Show($"{Message}\n There are {integerValue} characters in the message string.")
     End Sub
 End Class
