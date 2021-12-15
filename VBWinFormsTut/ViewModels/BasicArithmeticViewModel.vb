6c697a6f46bdd270f1c73a86ea04052086b24466
Marco Preciado <mac46005@gmail.com>
2021-12-14 18:47:57 -0600

    Continue on arithmentic view model

---
+++ VBWinFormsTut/ViewModels/BasicArithmeticViewModel.vb
@@ -0,0 +1,30 @@
+﻿Public Class BasicArithmeticViewModel
+    Private _ValueOne As Integer
+    Private _ValueTwo As Integer
+    Private _arithmeticSymVal As Integer
+    Private ReadOnly listofops() As String = {"+", "-", "*", "/"}
+
+
+
+
+
+
+    ' constructor
+    Public Sub New(valOne As Integer, valTwo As Integer, arithmeticSymVal As String)
+        _ValueOne = valOne
+        _ValueTwo = valTwo
+        _arithmeticSymVal = arithmeticSymVal
+    End Sub
+    Private Function Execute() As Integer
+        If _arithmeticSymVal = listofops(0) Then
+        ElseIf _arithmeticSymVal = listofops(1) Then
+
+        ElseIf _arithmeticSymVal = listofops(2) Then
+        ElseIf _arithmeticSymVal = listofops(3) Then
+        ElseIf _arithmeticSymVal = listofops(4) Then
+
+        End If
+    End Function
+
+
+End Class
